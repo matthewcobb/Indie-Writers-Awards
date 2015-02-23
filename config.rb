f6c40e9d3b256e6c8ce0bb5c635a9e8c22ad8179
Matt Cobb <contact@matthewcobb.co.uk>
2015-02-23 20:02:26 +0000

    Implement Bootstrap SASS and implement bootstrap col-ms

--- config.rb
+++ config.rb
@@ -1,12 +1,13 @@
 require 'compass/import-once/activate'
+require 'bootstrap-sass'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
